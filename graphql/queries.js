c26d6c6624ccbfb0a3cfad012ec6dd3e70b36d44
David Thorpe <davidthorpe878@gmail.com>
2019-05-21 21:50:23 +0100

    First page queries setup

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,5 +1,15 @@
 import gql from "graphql-tag";
 
+export const GET_ALL_PLANTS = gql`
+  query getPlants {
+    getPlants {
+      name
+      permalink
+      light
+    }
+  }
+`;
+
 export const GET_PLANT_BY_PERMALINK = gql`
   query getPlant($permalink: String) {
     getPlant(permalink: $permalink) {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { Query } from "react-apollo";
+import { GET_ALL_PLANTS } from "../graphql/queries";
 
 const Title = styled.h1`
   font-family: "Roboto";
@@ -7,4 +9,29 @@ const Title = styled.h1`
   color: ${({ theme }) => theme.colors.primary};
 `;
 
-export default () => <Title>Plant site</Title>;
+export default () => (
+  <Query query={GET_ALL_PLANTS}>
+    {({ data, loading, error }) => {
+      if (loading) {
+        return <p>Loading</p>;
+      }
+      if (error) {
+        return <p>Error</p>;
+      }
+      const { getPlants } = data;
+      return (
+        <div className="plants-container">
+          <Title>Plant site</Title>
+          {getPlants.map(item => (
+            <div className="each-plant" key="item.permalink">
+              <a href={`/${item.permalink}`}>
+                <h1>{item.name}</h1>
+              </a>
+              <p>light needs: {item.light}</p>
+            </div>
+          ))}
+        </div>
+      );
+    }}
+  </Query>
+);
